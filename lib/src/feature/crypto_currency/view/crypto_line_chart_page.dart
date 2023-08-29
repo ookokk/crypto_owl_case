ccff21415ea14cccc2b10b1ef614200731179666
osmanoscar <osmankok701@gmail.com>
2023-08-29 15:53:13 +0300

    line chart logic

--- lib/src/feature/crypto_currency/view/crypto_line_chart_page.dart
+++ lib/src/feature/crypto_currency/view/crypto_line_chart_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:crypto_owl/src/feature/crypto_currency/model/crypto_currency.dart'; // CryptoCurrency ve Data sınıflarınızın import edildiğinden emin olun
+import 'package:crypto_owl/src/feature/crypto_currency/model/crypto_currency.dart';
 
 class CryptoLineChartPage extends StatelessWidget {
   final List<Data> cryptoCurrencies;
@@ -12,33 +12,53 @@ class CryptoLineChartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Text('Change Percent 24 Hour'),
+        title: const Text('Change Percent 24 Hour'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: LineChart(
-          LineChartData(
-            gridData: const FlGridData(show: false),
-            titlesData: const FlTitlesData(show: false),
-            borderData: FlBorderData(show: false),
-            minX: 0,
-            maxX: cryptoCurrencies.length.toDouble() - 50,
-            minY: -2,
-            maxY: 2,
-            lineBarsData: [
-              LineChartBarData(
-                spots: cryptoCurrencies.asMap().entries.map((entry) {
-                  final data = entry.value;
-                  final y = double.parse(data.changePercent24Hr ?? "0");
-                  return FlSpot(entry.key.toDouble(), y);
-                }).toList(),
-                isCurved: true,
-                color: const Color(0xff183D3D),
-                dotData: const FlDotData(show: false),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300, // Adjust the height as needed
+            child: LineChart(
+              LineChartData(
+                gridData: const FlGridData(show: false),
+                titlesData: const FlTitlesData(show: false),
+                borderData: FlBorderData(show: false),
+                minX: 0,
+                maxX: cryptoCurrencies.length.toDouble() - 70,
+                minY: -2,
+                maxY: 2,
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: cryptoCurrencies.asMap().entries.map((entry) {
+                      final data = entry.value;
+                      final y = double.parse(data.changePercent24Hr ?? "0");
+                      return FlSpot(entry.key.toDouble(), y);
+                    }).toList(),
+                    isCurved: true,
+                    color: const Color(0xff183D3D),
+                    dotData: const FlDotData(show: false),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          SizedBox(
+            height: 150,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                final crypto = cryptoCurrencies[index];
+                return Container(
+                  height: 100,
+                  width: 50,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(child: Text(crypto.symbol ?? '')),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
